812be9e4c9ff3ebfab26a83520ee352d943b980c
David Craven <david@craven.ch>
2014-06-27 12:18:59 +0200

    Added functions for parsing input directory and test data

--- main/vdr2mp.py
+++ main/vdr2mp.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+from os import listdir
+from os.path import isfile, isdir, join
+from sys import argv
 
 
 def stringify(date_time):
@@ -13,3 +16,31 @@ def start_time(unix_time):
 
 def end_time(date_time, duration):
     return date_time + timedelta(seconds=duration)
+
+
+def read_file(path):
+    if isfile(path):
+        return open(path, 'r').read()
+
+
+def get_info_and_ts_files(wdir):
+    infofile = join(wdir, 'info')
+    tsfiles = []
+
+    if not isfile(infofile):
+        raise Exception('No info file found')
+
+    for tsfile in listdir(wdir):
+        if tsfile.endswith('.ts'):
+            tsfiles.append(join(wdir, tsfile))
+
+    return infofile, tsfiles
+
+
+if __name__ == '__main__':
+    INPUTDIR = argv[1]
+    OUTPUTDIR = argv[2]
+
+    for subdir in listdir(INPUTDIR):
+        if isdir(subdir):
+            get_info_and_ts_files(join(INPUTDIR, subdir))
--- tests/testvdr2mp.py
+++ tests/testvdr2mp.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from os.path import dirname, realpath, join
 from unittest import TestCase
 
-from main.vdr2mp import stringify, start_time, end_time
+from main.vdr2mp import stringify, start_time, end_time, get_info_and_ts_files
 
 
 class InfoFileParsingTestCase(TestCase):
@@ -22,3 +23,17 @@ class InfoFileParsingTestCase(TestCase):
 
     def test_end_time(self):
         self.assertEquals(end_time(self.start_time, self.duration), self.end_time)
+
+
+class InputdirParsingTestCase(TestCase):
+
+    tests_path = dirname(realpath(__file__))
+    inputdir_path = join(tests_path, 'inputdir')
+    folderdir_path = join(inputdir_path, 'Untitled.Folder')
+    infofile_path = join(folderdir_path, 'info')
+    tsfile1_path = join(folderdir_path, '00001.ts')
+    tsfile2_path = join(folderdir_path, '00002.ts')
+
+    def test_get_info_and_ts_files(self):
+        self.assertEquals(
+            get_info_and_ts_files(self.folderdir_path), (self.infofile_path, [self.tsfile1_path, self.tsfile2_path]))
