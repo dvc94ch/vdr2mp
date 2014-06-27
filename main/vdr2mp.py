f0b4b6745a021fb07fb602028d145ccd4fd85beb
David Craven <david@craven.ch>
2014-06-27 12:12:21 +0200

    Added functions for time conversions

--- main/vdr2mp.py
+++ main/vdr2mp.py
@@ -1,2 +1,15 @@
-def add(addend1, addend2):
-    return addend1 + addend2
+from datetime import datetime, timedelta
+
+
+def stringify(date_time):
+    if hasattr(date_time, 'strftime'):
+        return date_time.strftime('%Y-%m-%d %H:%M')
+    return str(date_time)
+
+
+def start_time(unix_time):
+    return datetime.fromtimestamp(int(unix_time))
+
+
+def end_time(date_time, duration):
+    return date_time + timedelta(seconds=duration)
--- tests/testvdr2mp.py
+++ tests/testvdr2mp.py
@@ -1,7 +1,24 @@
+from datetime import datetime
 from unittest import TestCase
-from main.vdr2mp import add
+
+from main.vdr2mp import stringify, start_time, end_time
 
 
-class MainTestCase(TestCase):
-    def test_add(self):
-        assert add(2, 3) == 5
+class InfoFileParsingTestCase(TestCase):
+
+    unix_time = 1403618279
+    duration = 4500
+    start_time = datetime(2014, 6, 24, 15, 57, 59)
+    end_time = datetime(2014, 6, 24, 17, 12, 59)
+    stringified = '2014-06-24 15:57'
+
+    def test_stringify(self):
+        self.assertEquals(stringify(self.start_time), self.stringified)
+        self.assertEquals(stringify(self.stringified), self.stringified)
+        self.assertEquals(stringify(5), '5')
+
+    def test_start_time(self):
+        self.assertEquals(start_time(self.unix_time), self.start_time)
+
+    def test_end_time(self):
+        self.assertEquals(end_time(self.start_time, self.duration), self.end_time)
